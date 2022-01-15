0481f3d93e2e83e33e4fdb73dc5b74ce3f5c5494
PriyankaB6 <priyanka.bhosaledangat@citiustech.com>
2022-01-15 18:03:41 +0530

    setup

---
+++ src/main/java/com/citius/appointmentms/AppointmentmsApplication.java
@@ -0,0 +1,13 @@
+package com.citius.appointmentms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppointmentmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/citius/appointmentms/AppointmentmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citius.appointmentms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
